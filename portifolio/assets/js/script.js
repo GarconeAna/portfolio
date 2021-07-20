70eb6b4028462fb7b74e7ed46a269c08b730b778
Ana Carolina <garcone.anaxx@gmail.com>
2021-07-20 19:33:50 -0300

    atualizando todos os arquivos

--- portifolio/assets/js/script.js
+++
@@ -1,9 +0,0 @@
-let inputNome = document.querySelector('#nome')
-
-inputNome.addEventListener('keyup', () => {
-  if(inputNome.value.length < 2){
-    inputNome.style.borderColor = 'red'
-  } else {
-    inputNome.style.borderColor = 'green'
-  }
-})
