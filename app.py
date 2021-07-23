e8c09c717b3cc53a086f50bdf305e260d361d942
Ana Carolina <garcone.anaxx@gmail.com>
2021-07-23 16:59:21 -0300

    mudando os arquivos de pasta

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ mail_settings = {
   'MAIL_PORT': '465',
   'MAIL_USE_TLS': False,
   'MAIL_USE_SSL': True,
-  'MAIL_USERNAME': 'gcarol.portfolio@gmail.com',
-  'MAIL_PASSWORD': 'g.anacarolina@portfolio'
+  'MAIL_USERNAME': 'gcarolina.portfolio@gmail.com',
+  'MAIL_PASSWORD': '@000gcarolina'
 }
 
 app.config.update(mail_settings)
@@ -21,10 +21,34 @@ class Contato:
     self.email = email
     self.mensagem = mensagem
 
+@app.route('/')
+def index():
+  return render_template('index.html')
+
+
 @app.route('/send', methods = ['GET', 'POST'])
 def send():
+  if request.method == 'POST' :
     formContato = Contato(
       request.form['nome'],
       request.form['email'],
       request.form['mensagem']
     )
+
+    msg = Message(
+      subject= 'Contato do seu potfólio',
+      sender= app.config.get('MAIL_USERNAME'),
+      recipients= [app.config.get('MAIL_USERNAME')],
+
+      body= f'''{formContato.nome} com o email {formContato.email}, te mandou a seguinte mensagem:
+            
+                {formContato.mensagem}'''
+      )
+      
+    mail.send(msg)
+  
+  return render_template ('send.html', formContato = formContato)
+
+
+if __name__ == '__main__' :
+  app.run(debug=True)
