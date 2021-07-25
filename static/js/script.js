0886cab5894e57c62122b39b5d1708603c067193
Ana Carolina <garcone.anaxx@gmail.com>
2021-07-25 18:20:07 -0300

    projeto atualizado com minhas informações

--- static/js/script.js
+++ static/js/script.js
@@ -15,38 +15,38 @@ let mensagemOk = false
 elementoBotao.addEventListener('click', () =>{
   if(elementoBotao.value == 'primeiro'){
     elementoImagem.src = '../static/img/portfolio.png'
-    elementoProjeto.innerText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis incidunt dolores distinctio harum hic ex corrupti.'
-    elementoLink.href = 'https://github.com'
+    elementoProjeto.innerText = "O projeto 'Portfolio' é este site. O site foi usado tudo o que aprendi no módulo 2 e python do módulo 1 e no final o deploy."
+    elementoLink.href = 'https://github.com/GarconeAna/portfolio'
     elementoBotao.value = 'segundo'
 
   } else if(elementoBotao.value == 'segundo'){
     elementoImagem.src = '../static/img/crime-scene.png'
-    elementoProjeto.innerText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis incidunt dolores distinctio harum hic ex corrupti.'
-    elementoLink.href = 'https://github.com'
+    elementoProjeto.innerText = "O projeto 'Jogo do Detetive' em python. Primeiro projeto do módulo 1, as perguntas feitas retorna sua classificação no crime"
+    elementoLink.href = 'https://github.com/GarconeAna/blueVS/blob/master/anaCarolinaGarcone-bluemod1-projeto1/projeto1Vscode-anaCarolinaGarcone.py'
     elementoBotao.value = 'terceiro'
 
   } else if(elementoBotao.value == 'terceiro'){
     elementoImagem.src = '../static/img/vote.png'
-    elementoProjeto.innerText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis incidunt dolores distinctio harum hic ex corrupti.'
-    elementoLink.href = 'https://github.com'
+    elementoProjeto.innerText = "O projeto 'Votação' feito em python, recebe os votos de três candidatos e no final mostra os resultados, simulando uma urna."
+    elementoLink.href = 'https://github.com/GarconeAna/blueVS/blob/master/anaCarolinaGarcone-bluemod1-projeto3/projeto3-anaCarolinaGarcone.py'
     elementoBotao.value = 'quarto'
 
   } else if(elementoBotao.value == 'quarto'){
     elementoImagem.src = '../static/img/jokenpo.png'
-    elementoProjeto.innerText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis incidunt dolores distinctio harum hic ex corrupti.'
-    elementoLink.href = 'https://github.com'
+    elementoProjeto.innerText = "O projeto 'Jokênpo' em python, é o jogo pedra, papel ou tesoura. Escolha quantas rodadas e no final saiba o grande vencedor."
+    elementoLink.href = 'https://github.com/GarconeAna/blueVS/blob/master/anaCarolinaGarcone-bluemod1-projeto2/projeto2-anaCarolinaGarcone.py'
     elementoBotao.value = 'quinto'
 
   } else if(elementoBotao.value == 'quinto'){
     elementoImagem.src = '../static/img/choice.png'
-    elementoProjeto.innerText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis incidunt dolores distinctio harum hic ex corrupti.'
-    elementoLink.href = 'https://github.com'
+    elementoProjeto.innerText = "O projeto 'Comeu Morreu' em python, é um jogo interativo feito em grupo onde cada uma das escolhas tem resultados diferente."
+    elementoLink.href = 'https://github.com/guilherme-correa-s/projeto-final'
     elementoBotao.value = 'sexto'
 
   } else {
     elementoImagem.src = '../static/img/face.png'
-    elementoProjeto.innerText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis incidunt dolores distinctio harum hic ex corrupti.'
-    elementoLink.href = 'https://github.com'
+    elementoProjeto.innerText = "O projeto 'Jogo do Humor' mostra um personagem e suas emoções. O primeiro projeto do módulo 2 usando html, css e javascript."
+    elementoLink.href = 'https://github.com/GarconeAna/personagem'
     elementoBotao.value = 'primeiro'
   }
 })
